dc3aa19f5fdab0e8b2bef7fd84dacb3c39631003
josehteixeira <joseehteixeira@gmail.com>
2022-05-08 22:06:28 -0300

    back-end adjusted for deploy

--- src/use-cases/submit-feedback-use-case.ts
+++ src/use-cases/submit-feedback-use-case.ts
@@ -41,6 +41,7 @@ export class SubmitFeedbackUseCase {
                     `<div style="font-family: sans-sefif; font-size: 16px; color: #111">`,
                     `<p>Tipo do feedback: ${type}</p>`,
                     `<p>Comentário: ${comment}</p>`,
+                    screenshot ? `<img src="${screenshot}" />` : null,
                     `</div>`,
                 ].join('\n')
         })
